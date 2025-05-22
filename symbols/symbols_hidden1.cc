8eea3f1450d6ee47a72d431c987550985e6c4a2a
anilhbtiit <CHAHAL.AC@GMAIL.COM>
2025-05-22 23:26:27 +0530

    Create symbols_hidden1.cc

---
+++ symbols/symbols_hidden1.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#define HIDE __declspec(dllimport)
+#else
+#define EXPORT __attribute__((visibility("default")))
+#define HIDE __attribute__((visibility("hidden")))
+#endif
+
+HIDE int inc = 0;
+int open = 0;
+
+class HIDE symbol {
+  public:
+  symbol() { mVal = 1; }
+  ~symbol() {}
+  int getInc() { return inc; }
+  int getVal() { return mVal; }
+  private:
+  int mVal;
+};
+
+
+HIDE void internal_function() {
+    inc++;
+    std::cout << "Internal function called\n";
+}
+
+//__attribute__((visibility("hidden"))) void public_function() {
+void public_function() {
+    inc++;
+    std::cout << "Public function called\n";
+}
+
+__attribute__((visibility("hidden"))) static void static_function() {
+    inc++;
+    std::cout << "Static function called\n";
+}
+
+int
+main() {
+  int val = 0;
+  static int ret = 0;
+  internal_function();
+  public_function();
+  static_function();
+  inc++;
+  std::cout << "inc: " << inc;
+  symbol sym;
+  std::cout << "symbol::mVal = " << sym.getVal();
+}
+
